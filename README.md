c68ee269ac2d9b232f94ac3733e713c70ec7128f
fu654 <admin@frozenutopia.net>
2023-12-12 19:38:24 +0900

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 - クレジット
     - [VOICEVOX ENGINE](https://github.com/VOICEVOX/voicevox_engine)
 - 起動
-```bash
-docker-compose up -d
-python main.py
-```
+    ```bash
+    docker-compose up -d
+    python main.py
+    ```
+
+- 環境構築(windows)
+    1. Pythonのインストール
+        ```powershell
+        pyenv local 3.11.6
+        python -m venv venv
+        .\venv\Scripts\activate
+        pip install -r requirements.txt
+        ```
+    1. `.env`ファイルを作成してDiscordのbotのトークンをセットする
+        ```
+        DISCORD_BOT_TOKEN=""
+        ```
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from discord import app_commands
-from discord.ext import commands
+# from discord.ext import commands
 from voicevox import create_wav_sound
 from typing import Final
 
@@ -28,6 +28,7 @@ async def test(interaction: discord.Interaction) -> None:
     username = interaction.user.name
     await interaction.response.send_message(f'テストコマンドを実行しました (receive from: {username})', ephemeral=False) # Trueにすると実行者のみ
 
+# 開始コマンド
 @tree.command(name="start",description="botをVCに参加させ音声読み上げを開始する")
 async def system_start(interaction: discord.Interaction) -> None:
     if not interaction.user.voice:
@@ -42,6 +43,7 @@ async def system_start(interaction: discord.Interaction) -> None:
     except:
         await interaction.response.send_message('ボイスチャンネルへの接続に失敗しました', ephemeral=False)
 
+# 終了コマンド
 @tree.command(name="stop",description="botをVCから退席させ音声読み上げを終了する")
 async def system_stop(interaction: discord.Interaction) -> None:
     if not interaction.guild.voice_client:
@@ -50,6 +52,7 @@ async def system_stop(interaction: discord.Interaction) -> None:
     await interaction.guild.voice_client.disconnect()
     await interaction.response.send_message('退席しました', ephemeral=False)
 
+# そのほかのメッセージ受信時
 @client.event
 async def on_message(message) -> None:
     if message.author == client.user:
