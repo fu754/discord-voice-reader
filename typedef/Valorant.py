f0019eee5906797321de43906078797d945b376f
fu654 <admin@frozenutopia.net>
2024-01-24 22:58:33 +0900

    型定義修正、json読み込み仮

--- typedef/Valorant.py
+++ typedef/Valorant.py
@@ -1,31 +1,31 @@
-from enum import Enum, auto
+from enum import Enum
 
 class Rank(Enum):
-    Iron1:      1
-    Iron2:      2
-    Iron3:      3
-    Bronze1:    4
-    Bronze2:    5
-    Bronze3:    6
-    Silver1:    7
-    Silver2:    8
-    Silver3:    9
-    Gold1:      10
-    Gold2:      11
-    Gold3:      12
-    Platinum1:  13
-    Platinum2:  14
-    Platinum3:  15
-    Diamond1:   16
-    Diamond2:   17
-    Diamond3:   18
-    Ascendant1: 19
-    Ascendant2: 20
-    Ascendant3: 21
-    Immortal1:  22
-    Immortal2:  23
-    Immortal3:  24
-    Radiant:    25
+    Iron1       = 1
+    Iron2       = 2
+    Iron3       = 3
+    Bronze1     = 4
+    Bronze2     = 5
+    Bronze3     = 6
+    Silver1     = 7
+    Silver2     = 8
+    Silver3     = 9
+    Gold1       = 10
+    Gold2       = 11
+    Gold3       = 12
+    Platinum1   = 13
+    Platinum2   = 14
+    Platinum3   = 15
+    Diamond1    = 16
+    Diamond2    = 17
+    Diamond3    = 18
+    Ascendant1  = 19
+    Ascendant2  = 20
+    Ascendant3  = 21
+    Immortal1   = 22
+    Immortal2   = 23
+    Immortal3   = 24
+    Radiant     = 25
 
 class Member():
     name: str
--- valorant.py
+++ valorant.py
@@ -1,5 +1,5 @@
 import json
-from typedef.Valorant import Member, Team
+from typedef.Valorant import Member, Team, Rank
 from LogController import get_logger
 from logging import Logger
 from typing import Final
@@ -14,8 +14,24 @@ def load_team_member_json() -> list[Member]:
         list[Member] : メンバー一覧
     """
     with open('./src/valorant/team_members.json', mode='r', encoding='utf_8_sig') as fp:
-        member_list = json.load(fp)
+        __member_list = json.load(fp)
+    member_list = []
+    for member in __member_list['members']:
+        name = member['name']
+        rank = member['rank']
+
+        m = Member(
+            name=name,
+            rank=Rank[rank]
+        )
+        member_list.append(m)
     return member_list
 
 def assign_team(member_list: list[Member]) -> Team:
     return {}
+
+
+if __name__ == '__main__':
+    member_list: list[Member] = load_team_member_json()
+    for m in member_list:
+        print(m.__dict__)
